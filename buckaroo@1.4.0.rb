e7d7db1ac59ba40e83108fb1c37ae1c468a4ed72
Vladislav Doronin <emelyk.vlad@yandex.ru>
2021-01-03 18:03:20 +0300

    Update buckaroo@1.4.0.rb

--- buckaroo@1.4.0.rb
+++ buckaroo@1.4.0.rb
@@ -12,7 +12,7 @@ class BuckarooAT140 < Formula
   head "https://github.com/LoopPerfect/buckaroo.git",
     :using => :git
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk@8"
   depends_on "buck"
 
   def install
